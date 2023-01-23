1b8332abeb356535e5c49320946fa502ddec76b2
Vitor <vitor.fgaletti@gmail.com>
2023-01-23 11:33:34 -0300

    ✨ Aula 05

---
+++ mobile/App.tsx
@@ -0,0 +1,37 @@
+import "./src/lib/dayjs";
+
+import {
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_800ExtraBold,
+  useFonts,
+} from "@expo-google-fonts/inter";
+import { StatusBar } from "react-native";
+
+import { Loading } from "./src/components/Loading";
+import { Routes } from "./src/routes";
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_800ExtraBold,
+  });
+
+  if (!fontsLoaded) {
+    return <Loading />;
+  }
+
+  return (
+    <>
+      <Routes />
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+    </>
+  );
+}
